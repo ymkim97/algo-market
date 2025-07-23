21b9ffff15e89f5be92d5601dcca811d64500507
ymkim97 <rladudaud97@naver.com>
2025-07-23 22:31:19 +0900

    init project

---
+++ problem-service/src/test/java/algomarket/problemservice/ProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package algomarket.problemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
