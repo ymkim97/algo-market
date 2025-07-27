f4ad850b38346149ae9f704d4751d530125e009f
ymkim97 <rladudaud97@naver.com>
2025-07-27 17:22:21 +0900

    config: Validation 추가

--- problem-service/build.gradle.kts
+++ problem-service/build.gradle.kts
@@ -28,6 +28,7 @@ val mockitoAgent: Configuration = configurations.create("mockitoAgent")
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
