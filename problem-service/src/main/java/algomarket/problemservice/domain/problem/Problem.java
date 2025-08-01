3dec49c221c41b89cb3df261ad69f2ec4be670c1
ymkim97 <rladudaud97@naver.com>
2025-08-01 23:15:37 +0900

    feat: Problem Entity 속성 추가

--- problem-service/src/main/java/algomarket/problemservice/domain/problem/Problem.java
+++ problem-service/src/main/java/algomarket/problemservice/domain/problem/Problem.java
@@ -4,28 +4,45 @@ import static org.springframework.util.Assert.state;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Entity
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Problem {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false,  unique = true, length = 100)
 	private String title;
 
+	@Column(nullable = false, columnDefinition = "MEDIUMTEXT")
 	private String description;
 
+	@Column(nullable = false)
 	private Integer submitCount;
 
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private ProblemStatus problemStatus;
 
 	// Second
+	@Column(nullable = false)
 	private Double timeLimit;
 
 	// MB
+	@Column(nullable = false)
 	private Integer memoryLimit;
 
 	public static Problem create(ProblemCreateRequest createRequest) {
